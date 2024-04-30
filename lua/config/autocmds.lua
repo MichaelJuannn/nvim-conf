b33e59b15a45d02d2a490780059a4d1d9519535f
MichaelJuannn <juan600d@gmail.com>
2024-04-30 10:06:40 +0700

    laravel treesitter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,31 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local filetypes = {
+  "html",
+  "javascript",
+  "typescript",
+  "javascriptreact",
+  "typescriptreact",
+  "svelte",
+  "vue",
+  "tsx",
+  "jsx",
+  "rescript",
+  "xml",
+  "php",
+  "markdown",
+  "astro",
+  "glimmer",
+  "handlebars",
+  "hbs",
+  "blade",
+}
+
 require("lspconfig").phpactor.setup({
   filetypes = { "php", "blade" },
+})
+
+require("nvim-ts-autotag").setup({
+  filetypes = filetypes,
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,21 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  opts = {
+    highlight = { enable = true },
+  },
   config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      ---@type table<string, boolean>
+      local added = {}
+      opts.ensure_installed = vim.tbl_filter(function(lang)
+        if added[lang] then
+          return false
+        end
+        added[lang] = true
+        return true
+      end, opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
     parser_config.blade = {
       install_info = {
