8890b431078f62ca4b2850e901852b7e041dcd72
MichaelJuannn <juan600d@gmail.com>
2024-08-01 19:31:12 +0700

    use neovim treesitter instead

--- lua/plugins/minai.lua
+++ lua/plugins/minai.lua
@@ -4,7 +4,7 @@ return {
     local ai = require("mini.ai")
     return {
       custom_textobjects = {
-        t = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+        t = false,
       },
     }
   end,
